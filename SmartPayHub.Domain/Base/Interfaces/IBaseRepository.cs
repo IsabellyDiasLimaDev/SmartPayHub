33967c086ee8c8a65452f3a08315af259aa132c8
IsabellyDiasLimaDev <isabelly.developer@gmail.com>
2025-07-04 16:15:42 -0300

    Adiciona IBaseRepository e atualiza repositórios
    Implementa a interface `IBaseRepository` com métodos assíncronos para operações CRUD. A interface `IMerchantRepository` agora herda de `IBaseRepository<MerchantEntity>`, removendo métodos específicos. Inclui referências ao Entity Framework Core no projeto e atualiza a solução para adicionar o projeto `SmartPayHub.Data`.

---
+++ SmartPayHub.Domain/Base/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmartPayHub.Domain.Base.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity?> GetByIdAsync(Guid id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        Task<TEntity?> UpdateAsync(TEntity entity);
+        Task DeleteAsync(Guid id);
+        Task<bool> ExistsAsync(Guid id);
+    }
+}
--- SmartPayHub.Domain/Interfaces/IMerchantRepository.cs
+++ SmartPayHub.Domain/Interfaces/IMerchantRepository.cs
@@ -1,16 +1,9 @@
-﻿using SmartPayHub.Domain.Entities;
+﻿using SmartPayHub.Domain.Base.Interfaces;
+using SmartPayHub.Domain.Entities;
 
 namespace SmartPayHub.Domain.Interfaces
 {
-    public interface IMerchantRepository
+    public interface IMerchantRepository : IBaseRepository<MerchantEntity>
     {
-        Task<IEnumerable<MerchantEntity>> GetAllMerchantsAsync();
-        Task<MerchantEntity> GetMerchantByIdAsync(Guid merchantId);
-        Task<IEnumerable<MerchantEntity>> GetMerchantsByUserIdAsync(Guid userId);
-        Task AddMerchantAsync(MerchantEntity merchant);
-        Task UpdateMerchantAsync(MerchantEntity merchant);
-        Task DeleteMerchantAsync(Guid merchantId);
-        Task<bool> MerchantExistsAsync(Guid merchantId);
-        Task<bool> MerchantNameExistsAsync(string name);
     }
 }
